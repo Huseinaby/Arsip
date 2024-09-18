bc41e7629a93d21061ff4637835c69e0e017391d
Abu Husein <huseinabu01@gmail.com>
2024-09-18 11:08:14 +0800

    add imb func

--- database/migrations/2024_09_18_010601_create_imb_table.php
+++ database/migrations/2024_09_18_010601_create_imb_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('imb', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
+            $table->string('nomor_dp');
             $table->text('alamat');
             $table->text('lokasi');
             $table->string('box');
