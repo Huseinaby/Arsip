e29f6d08e5eab3ff0858840cd90f9e5ea9207f4d
mhmmdazh <c030322053@mahasiswa.poliban.ac.id>
2024-09-19 14:42:59 +0800

    upadte management

--- resources/views/management.blade.php
+++ resources/views/management.blade.php
@@ -69,7 +69,7 @@
                 <span class="sr-only">Search</span>
             </button>
         </div>
-        <button type="button"
+        <button type="button" onclick="printTable()"
             class="flex items-center text-white bg-gray-800 border border-gray-300 focus:outline-none hover:bg-gray-600 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
             <svg class="w-6 h-6 text-white mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 24 24">
@@ -79,12 +79,13 @@
             PRINT ALL
         </button>
 
+
     </div>
 
 </form>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table id="dataTable" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">NO DP</th>
@@ -94,58 +95,61 @@
                 <th scope="col" class="px-6 py-3">keterangan</th>
                 <th scope="col" class="px-6 py-3">Boks</th>
                 <th scope="col" class="px-6 py-3">Tahun</th>
-                <th scope="col" class="px-6 py-3">Lihat</th>
-                <th scope="col" class="px-6 py-3">Edit</th>
-                <th scope="col" class="px-6 py-3">Print</th>
+                <th scope="col" class="px-6 py-3 hidden-print">Lihat</th>
+                <th scope="col" class="px-6 py-3 hidden-print">Edit</th>
+                <th scope="col" class="px-6 py-3 hidden-print">Print</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($items as $item)
-            <tr
-                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{ $item->nomor_dp }}
-                </th>
-                <td class="px-6 py-4">{{ $item->nama }}</td>
-                <td class="px-6 py-4">{{ $item->alamat }}</td>
-                <td class="px-6 py-4">{{ $item->lokasi }}</td>
-                <td class="px-6 py-4">{{ $item->keterangan }}</td>
-                <td class="px-6 py-4">{{ $item->box }}</td>
-                <td class="px-6 py-4">{{ $item->tahun }}</td>
-                <td class="px-6 py-4">
-                    <svg id="eye-open" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"
-                        style="cursor: pointer;">
-                        <path stroke="currentColor" stroke-width="2"
-                            d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
-                        <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" onclick="openEditModal({{ json_encode($item) }})"
-                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                <tr
+                    class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $item->nomor_dp }}
+                    </th>
+                    <td class="px-6 py-4">{{ $item->nama }}</td>
+                    <td class="px-6 py-4">{{ $item->alamat }}</td>
+                    <td class="px-6 py-4">{{ $item->lokasi }}</td>
+                    <td class="px-6 py-4">{{ $item->keterangan }}</td>
+                    <td class="px-6 py-4">{{ $item->box }}</td>
+                    <td class="px-6 py-4">{{ $item->tahun }}</td>
+                    <td class="px-6 py-4 hidden-print">
+                        <svg id="eye-open" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"
+                            style="cursor: pointer;">
+                            <path stroke="currentColor" stroke-width="2"
+                                d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                            <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
-                    </a>
+                    </td>
+                    <td class="px-6 py-4 hidden-print">
+                        <a href="#" onclick="openEditModal({{ json_encode($item) }})"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                            </svg>
+                        </a>
 
-                </td>
+                    </td>
 
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><svg
-                            class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linejoin="round" stroke-width="2"
-                                d="M16.444 18H19a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h2.556M17 11V5a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v6h10ZM7 15h10v4a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-4Z" />
-                        </svg>
-                    </a>
-                </td>
-            </tr>
+                    <td class="px-6 py-4 hidden-print">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><svg
+                                class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linejoin="round" stroke-width="2"
+                                    d="M16.444 18H19a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h2.556M17 11V5a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v6h10ZM7 15h10v4a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-4Z" />
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
+    <div>
+
+    </div>
 </div>
 <div id="editModal" class="fixed inset-0 z-50 flex items-center justify-center hidden bg-gray-800 bg-opacity-50">
     <div class="bg-gray-100 p-6 rounded-lg shadow-lg border">
@@ -165,7 +169,7 @@
                         class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600">Nomor
                         DP</label>
                     @if ($errors->has('nomor_dp'))
-                    <p class="text-red-500 text-xs mt-2">{{ $errors->first('nomor_dp') }}</p>
+                        <p class="text-red-500 text-xs mt-2">{{ $errors->first('nomor_dp') }}</p>
                     @endif
                 </div>
 
@@ -239,6 +243,10 @@
     </div>
 </div>
 
+<div class="mt-10">
+    {{ $items->links() }}
+</div>
+
 
 <script>
     function openEditModal(item) {
@@ -260,7 +268,7 @@
     }
 
     document.querySelectorAll('.filter-item').forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             // Ganti teks tombol filter dengan nilai dari data-filter
             document.getElementById('dropdown-button').innerHTML = `${this.dataset.filter} <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>`;
 
@@ -270,8 +278,45 @@
     });
 
     // Fungsi untuk toggle dropdown visibility
-    document.getElementById('dropdown-button').addEventListener('click', function() {
+    document.getElementById('dropdown-button').addEventListener('click', function () {
         document.getElementById('dropdown').classList.toggle('hidden');
     });
+
+    function printTable() {
+        // Mendapatkan elemen tabel
+        var table = document.getElementById('dataTable').outerHTML;
+
+        // Membuat jendela baru untuk print
+        var printWindow = window.open('', '', 'height=800,width=1200');
+
+        // Menulis HTML ke jendela print
+        printWindow.document.write('<html><head><title>Data IMB</title>');
+        printWindow.document.write('<style>table { width: 100%; border-collapse: collapse; } th, td { border: 1px solid black; padding: 8px; } th { background-color: #f2f2f2; } @media print { .hidden-print { display: none !important; } }</style>');
+        printWindow.document.write('</head><body >');
+        printWindow.document.write(table);
+        printWindow.document.write('</body></html>');
+
+        // Menutup jendela print dan memanggil fungsi print
+        printWindow.document.close();
+        printWindow.focus();
+        printWindow.print();
+
+        setTimeout(function () {
+            printWindow.close();
+        }, 100);
+    }
+
+
+    // Menambahkan event listener untuk tombol print
+    document.querySelector('button[type="button"].font-medium.text-blue-600').addEventListener('click', printTable);
 </script>
+<style>
+    @media print {
+        .hidden-print {
+            display: none !important;
+        }
+    }
+</style>
+
+
 @endsection
