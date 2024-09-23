9a906c2838887b167099adc4124625a9068ea0cb
mhmmdazh <c030322053@mahasiswa.poliban.ac.id>
2024-09-23 09:16:11 +0800

    fitur print

--- resources/views/management.blade.php
+++ resources/views/management.blade.php
@@ -135,7 +135,8 @@
                     </td>
                     <td class="px-6 py-4 hidden-print">
                         <a href="#" class="print-pdf font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                            data-file="{{ asset("imbs/$item->imbs") }}">
+                        data-file="{{ asset('storage/imbs/' . $item->imbs) }}"
+                        >
                             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-linejoin="round" stroke-width="2"
