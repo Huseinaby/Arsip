6dd2083e0763c8c2ae47e32631e3d7f93e2ca9ed
Maulidin <mmaulidinabdi@mail.com>
2024-10-29 10:50:27 +0800

    jngn di anu

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,5 +1,5 @@
 @include('partials.header')
-<section class="bg-gray-50 dark:bg-gray-900">
+<section class="drop-shadow-lg bg-gray-50 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
         @if (session()->has('loginError'))
